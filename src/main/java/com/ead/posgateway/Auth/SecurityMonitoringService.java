411c5d15f519b677bf18f0909bbbf7bcf9acf494
Diniranga <dinirangapremanayake@gmail.com>
2025-07-15 14:54:40 +0530

    nit

--- src/main/java/com/ead/posgateway/Auth/SecurityMonitoringService.java
+++ src/main/java/com/ead/posgateway/Auth/SecurityMonitoringService.java
@@ -14,7 +14,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
-import com.ead.posgateway.User.User;
 
 /**
  * Service for monitoring and logging security-related events.
